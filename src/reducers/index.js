87afde5987d0a919a02898a1d3ead3f70a2db0a6
Lelsey Dreyer <lesleydreyer@hotmail.com>
2019-05-24 18:08:47 -0700

    initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import characters from './charactersReducer';
+import heroes from './heroesReducer';
+
+const rootReducer = combineReducers({
+    characters, heroes
+});
+
+export default rootReducer;
